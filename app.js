afa3d726c892a6a06995c271774af3d23ca3cfdd
Priya <49821685+ladybug007@users.noreply.github.com>
2020-08-18 13:38:53 -0500

    Express session-1

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+
+
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
+);
 
 var app = express();
 
@@ -19,7 +35,57 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+function auth(req, res, next) {
+console.log(req.session)
+  if (!req.session.user) {
+    
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+     // res.cookie('user', 'admin', { signed: true })
+     req.session.user = 'admin'
+      return next();
+    }
+    else {
+      const err = new Error('You are not authenticated!')
+      err.status = 401;
+      res.setHeader('WWW-Authenticate', 'Basic')
+      return next(err);
+    }
+  } else {
+    if (req.session.user === 'admin') {
+      return next();
+    }
+    else {
+      const err = new Error('You are not authenticated!')
+      err.status = 401;
+      res.setHeader('WWW-Authenticate', 'Basic')
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -29,12 +95,12 @@ app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,49 +1,80 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const partnerRouter = express.Router();
+const Partner = require('../models/partner');
 
+const partnerRouter = express.Router();
 partnerRouter.use(bodyParser.json());
 
 partnerRouter.route('/')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    next();
-})
-.get((req,res)=>{
-    res.end('Will send all partners to you')
-})
-.post((req,res)=>{
-    res.end(`Will add promotion: ${req.body.name} with description: ${req.body.description}`)
-})
-.put((req,res)=>{
-    res.statusCode = 403;
-    res.end(`PUT operation not supported on /partners`)
-})
-.delete((req,res)=>{
-    res.end(`Deleting all partners`)
-});
+    .get((req, res, next) => {
+        Partner.find()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+            })
+            .catch(err => next(err))
+    })
+    .post((req, res, next) => {
+        Partner.create(req.body)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+
+            })
+            .catch(err => next(err))
+    })
+
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end(`PUT operation not supported on /partners`)
+    })
+    .delete((req, res, next) => {
+        Partner.deleteMany()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+
+            })
+            .catch(err => next(err))
+    });
 
 partnerRouter.route('/:partnerId')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        next();
-    })
-    .get((req, res) => {
-        res.end(`Will send details of the partner: ${req.params.partnerId} to you `);
+
+    .get((req, res, next) => {
+        Partner.findById(req.params.partnerId)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+            })
+            .catch(err => next(err))
     })
     .post((req, res) => {
         res.statusCode = 403;
         res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
     })
     .put((req, res) => {
-        res.write(`Updating partner: ${req.params.partnerId}\n`);
-        res.end(`Will update partner: ${req.body.name} with description: ${req.body.description}`);
+        Partner.findByIdAndUpdate(req.params.partnerId, { $set: req.body }, { new: true })
+            .then(partner => {
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+            })
+            .catch(err => next(err))
     })
-    .delete((req, res) => {
-        res.end(`Deleting partner ${req.params.partnerId}`)
+    .delete((req, res, next) => {
+        Partner.findByIdAndDelete(req.params.partnerId)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner)
+            })
+            .catch(err => next(err))
     });
 
 
-    module.exports = partnerRouter;
+module.exports = partnerRouter;
